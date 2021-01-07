6455106ec7dcb049f99b1270a83b2d36520246b6
Milton Plasencia <milton@Macton.homenet.telecomitalia.it>
2021-01-07 20:16:13 +0100

    Sort2 report

--- Sort2/src/quick_sort.c
+++ Sort2/src/quick_sort.c
@@ -33,14 +33,34 @@ int* new_partition(void *A, const size_t elem_size, size_t i, size_t j ,size_t p
 	return k;	
 }
 
+unsigned int partition(void *A, const size_t elem_size, size_t i, size_t j ,size_t p, total_order_type leq)
+{
+	swap(A+i*elem_size, A+p*elem_size, elem_size);
+	p = i;
+	i++;
+	j--; //Non può avere come valore n !!!
+	while(i<=j){
+		if(leq(A+p*elem_size, A+i*elem_size)){
+			swap(A+i*elem_size,A+j*elem_size, elem_size);
+			j--;
+		}else{
+			i++;
+		}
+	}
+	swap(A+p*elem_size, A+j*elem_size, elem_size);
+	return j;	
+}
+
 
 void quick_sort_aux(void *A, size_t i, const unsigned int n, const size_t elem_size, total_order_type leq)
 {
-	int *k;
+	//int *k;
+	unsigned int k;
 	while(i<n){
-		k = new_partition(A, elem_size, i, n, i, leq);
-		quick_sort_aux(A, i, k[0], elem_size, leq);
-		i = k[1]+1; //tail recursion
+		k = partition(A, elem_size, i, n, i, leq);
+		quick_sort_aux(A, i, k, elem_size, leq);
+		//i = k[1]+1; //tail recursion
+		i = k+1;
 		//quick_sort_aux(A, p+1, n, elem_size, leq); 
 	}		
 }
